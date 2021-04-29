1464512a6c39d931444f7d0d5353ac500ddfa938
Dragma <musaevilkin29@gmail.com>
2021-04-29 17:56:19 +0400

    More fib

--- go-hello-world/main.go
+++ go-hello-world/main.go
@@ -1,12 +1,12 @@
 package main
 
 func main() {
-	const n = 10
+	const n = 20
 	d := [n + 1]int{}
 	d[0] = 1
 	d[1] = 1
 	for i := 2; i <= n; i++ {
 		d[i] = d[i-1] + d[i-2]
 	}
-	print(d[10])
+	print(d[n])
 }
