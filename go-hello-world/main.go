ccb3e8a0310df7a8c6ed70f8400027fcb5a6d6aa
Dragma <musaevilkin29@gmail.com>
2021-04-29 17:49:54 +0400

    Fib

--- go-hello-world/main.go
+++ go-hello-world/main.go
@@ -1,9 +1,12 @@
 package main
 
 func main() {
-	print("Hello world3")
-	print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11)
-	for {
-
+	const n = 10
+	d := [n + 1]int{}
+	d[0] = 1
+	d[1] = 1
+	for i := 2; i <= n; i++ {
+		d[i] = d[i-1] + d[i-2]
 	}
+	print(d[10])
 }
