5fd602bb79eee20675b2f221b8490cd8452b98ac
Dragma <musaevilkin29@gmail.com>
2021-05-08 19:29:28 +0400

    Fix

--- go-2/main.go
+++ go-2/main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/tamerlan", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"hello": "world2",
+			"hello": "world3",
 		})
 	})
 	r.Run("0.0.0.0:8000")
