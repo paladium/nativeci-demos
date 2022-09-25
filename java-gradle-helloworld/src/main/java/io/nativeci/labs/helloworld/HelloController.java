38c9012d52a11a67ed81d6085b4caf096f6f06ed
Dragma <musaevilkin29@gmail.com>
2022-09-25 11:07:39 +0400

    Add java helloworld

---
+++ java-gradle-helloworld/src/main/java/io/nativeci/labs/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package io.nativeci.labs.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
+
+}
